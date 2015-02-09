3840dc0bf5db2452276f6b74fb2c51c3216fd35b
Paul Kokoszyna <pkoko9@hotmail.com>
2015-02-09 15:55:12 -0600

    Add Function #1

--- homework/mocha-chai-setup/tests/tests.js
+++ homework/mocha-chai-setup/tests/tests.js
@@ -1,3 +1,5 @@
+// Test case #1
+
 var expect = chai.expect;
 
 describe('max', function (){
@@ -10,11 +12,12 @@ describe('max', function (){
 		expect(function(){ max (1,'b') }).to.throw('The second argument must be a number.');
 	});
 	it('Should return the max', function () {
-		expect(max(1,2)).to.equal(2);
 		expect(max(2,1)).to.equal(2);
+		expect(max(1,2)).to.equal(2);
 		expect(max(38,87)).to.equal(87);
 		expect(max(-4,12)).to.equal(12);
 		expect(max(-5,-17)).to.equal(-5);
-		expect(max(.23, .87)).to.equal(.87);
+		expect(max(.23,.87)).to.equal(.87);
+		expect(max(2,2)).to.equal(2 + ' and ' + 2 + ' are equal');
 	});
 });
